4d57101008c4ebafaaf4dd29fcc0a22517aea79c
bobeu <bobmatea27@gmail.com>
2021-04-15 14:15:41 +0100

    updated contract files

--- contracts/Plantashun.sol
+++ contracts/Plantashun.sol
@@ -27,6 +27,7 @@ abstract contract Plantashun {
     event TokenTransfer(address from);
     event RootPurchased(address indexed _buyer, uint256 _amount, uint _time);
     event SownSeed(address indexed _user, uint256 _amount, uint _time);
+    event Harvest(address indexed _harvester, uint256 _amount, uint256 _date);
 
     struct RootInvestor{
         bool isRootInvestor;
@@ -39,7 +40,7 @@ abstract contract Plantashun {
  
     mapping(address => uint256) germinator;
     mapping(address => RootInvestor) rootInvestorsMap;
-    mapping(uint8 => uint256) constant duration;
+    mapping(uint8 => uint256) duration;
     mapping(address => bool) public blacklist;
     mapping(uint256 => uint8) yield;
     
@@ -75,14 +76,16 @@ abstract contract Plantashun {
     }
 
     modifier hasEnoughRootBalance(uint _qty) {
-        require(_root.balanceOf[msg.sender] > _qty, "Insufficient balance"); _;
+        uint bal = _root.balanceOf(msg.sender);
+        require(bal > _qty, "Insufficient balance"); _;
     }
 
     modifier hasEnoughCropBalance(uint _qty) {
-        require(_crop.balanceOf[msg.sender] > _qty, "Insufficient balance"); _;
+         uint bal = _crop.balanceOf(msg.sender);
+        require(bal > _qty, "Insufficient balance"); _;
     }
 
-    function setPercentYield(uint8 _count, uint128 _p) public onlyOwner returns(bool) {
+    function setPercentYield(uint8 _count, uint8 _p) public onlyOwner returns(bool) {
         require(_count > 0 && _count <= 6, "Duration out of range");
         yield[_count] = _p;
         return true;
@@ -101,22 +104,22 @@ abstract contract Plantashun {
     }
 
     function getSeedBalance() public view returns(uint256) {
-        return _root.balanceOf[msg.sender];
+        _root.balanceOf(msg.sender);
     }
 
     function getCropBalance() public view returns(uint256) {
-        return _crop.balanceOf[msg.sender];
+        _crop.balanceOf(msg.sender);
     }
 
     function buyRoot(uint256 _qty) public {
         uint initialBalance = msg.sender.balance;
-        uint256 amountToPay = mul(_qty, seedPrice);
+        uint256 amountToPay = _qty * seedPrice;
         require(msg.sender.balance > amountToPay, "Insufficient fund");
-        sub(msg.sender.balance, amountToPay);
+        msg.sender.balance - amountToPay;
         address(this).transfer(amountToPay);
         require(address(this).balance > initialBalance, "Anomally detected: transaction reversed");
-        sub(_root.balanceOf[address(this)], _qty);
-        add(_root.balanceOf[msg.sender], _qty);
+        _root.balanceOf[address(this)] - _qty;
+        _root.balanceOf[msg.sender] + _qty;
         _root.allowance(msg.sender, address(this));
         _root.approve(address(this), _qty);
         rootInvestorsMap[msg.sender] = RootInvestor(true, 0, 0, false, 0);
@@ -129,8 +132,8 @@ abstract contract Plantashun {
     function sowSeed(uint _qty, uint8 _duration) public hasRoot hasEnoughRootBalance(_qty) returns(bool) {
         // require(rootInvestorsMap[msg.sender].lockGerminator == false, "Seed already germinated.");
         require(_duration > 0 && _duration <= 6, "Duration out of range");
-        sub(_root.balanceOf[msg.sender], _qty);
-        add(germinator[msg.sender], _qty);
+        _root.balanceOf[msg.sender] - _qty;
+        germinator[msg.sender] + _qty;
         uint256 k = duration[_duration];
         uint8 reward_base = yield[k];
         rootInvestorsMap[msg.sender].rewardBase = reward_base;
@@ -144,34 +147,20 @@ abstract contract Plantashun {
 
     function harvest() public hasRoot returns(bool, uint256) {
         require(germinator[msg.sender] > 0 && rootInvestorsMap[msg.sender].lockGerminator = true, "User have no stake");
-        require(block.timestamp >= add(rootInvestorsMap[msg.sender].sowTime, rootInvestorsMap[msg.sender].germ_duration), "Root not yet mature");
+        require(block.timestamp >= (rootInvestorsMap[msg.sender].sowTime + rootInvestorsMap[msg.sender].germ_duration, "Root not yet mature"));
         uint _s = germinator[msg.sender];
-        sub(germinator[msg.sender], _s);
+        germinator[msg.sender] - _s;
         uint _reward_b = rootInvestorsMap[msg.sender].rewardBase;
-        uint _reward = add(_s, mul(_s, _reward_b));
+        uint _reward = _s + (_s * _reward_b);
 
-        sub(_crop.balanceOf[address(this)], _reward);
-        add(_crop.balanceOf[msg.sender], _reward);
+        _crop.balanceOf[address(this)] - _reward;
+        _crop.balanceOf[msg.sender] + _reward;
         rootInvestorsMap[msg.sender].harvest = _reward;
-    }
 
-    function _setUserInfo(address _target, uint _reward, uint _rewardCount) internal {
-        holders.push(Investor(_target, true, _reward, _rewardCount));
+        emit Harvest(msg.sender, _reward, block.timestamp);
     }
     
     function changeOwnership() public {
-        
-    }
-    
-    function plantSeed(uint _amt) public returns(bool) {
-        
-    }
-    
-    function harvestCrop() public {
-        
-    }
-    
-    function buySeed() public {
         
     }
     
