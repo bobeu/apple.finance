5d47e6aa830dedbcfb847d2d85e84e6c76df9969
bobeu <bobmatea27@gmail.com>
2021-04-17 03:04:59 +0100

    updated contract files

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Plantashun = artifacts.require("Plantashun");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Plantashun);
 };
